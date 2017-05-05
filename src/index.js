d81601bb830df6efef9d3063cf21b4fe88ba64b3
Andrew Hamilton <andrew@lucidlive.com>
2017-05-05 16:22:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import ReactDOM from 'react-dom';
-import Chart from 'chart.js';
+import Chart from 'chartjs-internal';
 import isEqual from 'lodash.isequal';
 
 class ChartComponent extends React.Component {
